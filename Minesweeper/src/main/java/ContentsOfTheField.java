cd76e332220e3cb3aa613de8ed43efac348a5b9a
Sree <sree.vidhya@myob.com>
2019-11-04 14:02:22 +1100

    Contents of the field and tested

---
+++ Minesweeper/src/main/java/ContentsOfTheField.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ContentsOfTheField {
+    ArrayList AllTheConTentOfTheField;
+
+    public ContentsOfTheField(ArrayList allTheConTentOfTheField) {
+        AllTheConTentOfTheField = allTheConTentOfTheField;
+    }
+
+    public ArrayList getAllTheConTentOfTheField() {
+        return AllTheConTentOfTheField;
+    }
+}
